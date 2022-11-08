7cd0dc7147c0dc1fcd811b986a21a8a8c86b78d3
Pablo Gustavo Dietrich <pablogustavodietrich@gmail.com>
2022-11-08 12:19:29 -0300

    bordes

--- js/oradores.js
+++ js/oradores.js
@@ -1,17 +1,3 @@
-/*Trabajo Práctico Integrador JS
-
-Deberán replicar la siguiente sección (es una sección que se debe agregar al integrador front-End) al presionar en el enlace “tickets”.
-
-Deberá contener la funcionalidad en Javascript, al momento de presionar el botón “Resumen”, deberá mostrar en la sección “Total a Pagar: $”, el monto correspondiente a la cantidad de tickets a comprar con el descuento correspondiente dependiendo la categoría seleccionada, existen 3 categorías, Estudiante, Trainee, Junior
-*/
-
-
-
-
-
-
-
-
 const resumen = document.querySelector(".button_Resumen");
 const toltalAPagar = document.querySelector(".Total_a_pagar"); 
 const cantidad = document.querySelector(".Input_Cantidad");
@@ -63,14 +49,6 @@ function reset() {
     return toltalAPagar.innerHTML = `Total a pagar: $`;
 }
 
-resumen.addEventListener("click", mostrarPrecio); //EL EVENTO VA ENTRE COMILLAS Y LA FUNCION EL UNICO PARAMTRO QUE RECIBE ES EL: EVENTO;
+resumen.addEventListener("click", mostrarPrecio);
 borrar.addEventListener("click",reset);
 
-/*
-let cantidad = document.getElementsByClassName("Input_Cantidad"); //devuelve un array
-
-console.log(parseInt((cantidad[0].value)));
-
-let cantidad2 = document.querySelector(".Input_Cantidad"); //devuelve un valor
-
-console.log(parseInt((cantidad2.value))); */
